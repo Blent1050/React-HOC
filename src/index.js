0953950eaf179b001fa57e2ba8d0f36dfebba1d8
Brandon lent <blent1050@yahoo.com>
2019-01-16 12:00:51 -0500

    File completed - use for reference

--- src/index.js
+++ src/index.js
@@ -3,11 +3,49 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-const HelloWorld = () => <h1>Higher Order Components</h1>;
-const Button = () => <button className="btn btn-primary">Press Button</button>;
+const HelloWorld = props => (
+  <h1 onClick={props.increment}>
+    {props.hello} {props.count}
+  </h1>
+);
+const Button = props => (
+  <button className="btn btn-primary" onClick={props.increment}>
+    {props.count}
+  </button>
+);
 
+const higherOrderComponent = WrappedComponent =>
+  class extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        hello: "Higher Order Component",
+        counter: 0
+      };
+    }
+    addUpCounter = () => {
+      this.setState((prevState, props) => {
+        return {
+          counter: prevState.counter + 1
+        };
+      });
+    };
+
+    render() {
+      return (
+        <WrappedComponent
+          increment={this.addUpCounter}
+          hello={this.state.hello}
+          count={this.state.counter}
+        />
+      );
+    }
+  };
+
+const HOCHelloWorld = higherOrderComponent(HelloWorld);
+const HOCButton = higherOrderComponent(Button);
 const App = () => {
-  return [<HelloWorld key="1" />, <Button key="2" />];
+  return [<HOCHelloWorld key="1" />, <HOCButton key="2" />];
 };
 
 const rootElement = document.getElementById("root");
